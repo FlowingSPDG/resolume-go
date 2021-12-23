084b15b6a765f03c4388cd05f5e455f09a7fd343
Shugo Kawamura <kawamura@giken.tv>
2021-12-23 16:32:43 +0900

    Compositionに関連する二つのメソッドを追加

--- composition.go
+++ composition.go
@@ -3,6 +3,7 @@ package resolume
 import (
 	"context"
 
+	"github.com/FlowingSPDG/resolume-go/internal"
 	"github.com/FlowingSPDG/resolume-go/models"
 )
 
@@ -12,6 +13,21 @@ func (c *Client) ListComposition(ctx context.Context) (*models.Composition, erro
 	if err != nil {
 		return nil, err
 	}
-
 	return (*models.Composition)(res.JSON200), nil
+}
+
+// UpdateComposition Update the complete composition
+func (c *Client) UpdateComposition(ctx context.Context, body models.ReplaceCompositionJSONRequestBody) error {
+	if _, err := c.c.ReplaceCompositionWithResponse(ctx, internal.ReplaceCompositionJSONRequestBody(body)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) ResetComposition(ctx context.Context, param string, body models.ResetCompositionParamJSONRequestBody) error {
+	if _, err := c.c.ResetCompositionParamWithResponse(ctx, param, internal.ResetCompositionParamJSONRequestBody(body)); err != nil {
+		return err
+	}
+
+	return nil
 }
